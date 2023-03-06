2047ff30c169d57fbe720b93a2bd8130066fec23
Ayushkumar8969 <103050632+Ayushkumar8969@users.noreply.github.com>
2023-03-06 23:30:35 +0530

    Add files via upload

---
+++ SEARCHING/SquareRootNumber.java
@@ -0,0 +1,34 @@
+class SquareRootNumber {
+    public static int mySqrt(int x) {
+        if(x==0)
+        {
+            return 0;
+        }
+         
+         int start=1;
+         int end=x;
+         int ans=0;
+         while(start<=end)
+         {
+               int mid = start + (end-start)/2;
+               if(mid==x/mid)
+               {
+                   return mid;
+               }
+              else if(mid>x/mid)
+               {
+                   end=mid-1;
+               }
+               else
+               {
+                    ans=mid;
+                   start=mid+1;
+               }
+         }
+         return ans;
+        
+    }
+    public static void main(String[] args) {
+            System.out.println(mySqrt(2147483647));
+    }
+}
